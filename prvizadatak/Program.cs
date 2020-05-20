8daa2e65f36ce471157eb025077f5db7a24721d8
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 16:00:31 +0200

    .

--- prvizadatak/Program.cs
+++ prvizadatak/Program.cs
@@ -12,13 +12,22 @@ namespace RPPOONLV7
         {
             double[] myArray = new double[] { 72, 11, 65, 3, 9, -36, 14, 100, 0, -84 };
             NumberSequence mySequence = new NumberSequence(myArray);
-
-            BubbleSort myBubble = new BubbleSort();
-            mySequence.SetSortStrategy(myBubble);
-
+            mySequence.SetSortStrategy(new BubbleSort());
             Console.WriteLine("Niz koji treba sortirati:\n" + mySequence.ToString());
             mySequence.Sort();
-            Console.WriteLine("Sortirani niz:\n " + mySequence.ToString());
+            Console.WriteLine("Niz sortiran algoritmom Bubble sort:\n " + mySequence.ToString());
+
+            mySequence = new NumberSequence(myArray);
+            mySequence.SetSortStrategy(new CombSort());
+            Console.WriteLine("Niz koji treba sortirati:\n" + mySequence.ToString());
+            mySequence.Sort();
+            Console.WriteLine("Niz sortiran algoritmom Comb sort:\n " + mySequence.ToString());
+
+            mySequence = new NumberSequence(myArray);
+            mySequence.SetSortStrategy(new SequentialSort());
+            Console.WriteLine("Niz koji treba sortirati:\n" + mySequence.ToString());
+            mySequence.Sort();
+            Console.WriteLine("Niz sortiran algoritmom Sequential sort:\n " + mySequence.ToString());
 
 
         }

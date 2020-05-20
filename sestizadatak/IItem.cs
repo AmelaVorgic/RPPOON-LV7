8825009dc3898c3c5a18059d7c708ef0309c0746
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 13:49:09 +0200

    .

---
+++ sestizadatak/IItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOONLV7
+{
+    interface IItem
+    {
+        double Accept(IVisitor visitor);
+    }
+}

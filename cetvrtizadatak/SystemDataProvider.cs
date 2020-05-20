b4a1f759d9e6e47ebc80f038490a1eb82b634f85
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 12:23:36 +0200

    .

---
+++ cetvrtizadatak/SystemDataProvider.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOONLV7
+{
+    class SystemDataProvider : SimpleSystemDataProvider
+    {
+        private float previousCPULoad;
+        private float previousRAMAvailable;
+        public SystemDataProvider() : base()
+        {
+            this.previousCPULoad = this.CPULoad;
+            this.previousRAMAvailable = this.AvailableRAM;
+        }
+        public float GetCPULoad()
+        {
+            float currentLoad = this.CPULoad;
+            if (currentLoad != this.previousCPULoad)
+            {
+                this.Notify();
+            }
+            this.previousCPULoad = currentLoad;
+            return currentLoad;
+        }
+        public float GetAvailableRAM()
+        {
+            float currentLoad = this.AvailableRAM;
+            if (currentLoad > this.previousRAMAvailable * 1.1 || currentLoad < this.previousRAMAvailable * 0.9)
+            {
+                this.Notify();
+            }
+            this.previousRAMAvailable = currentLoad;
+            return currentLoad;
+        }
+    }
+}

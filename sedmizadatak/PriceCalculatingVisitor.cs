9bb72b2911e20c02a551835242e4778b7153c256
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 14:12:39 +0200

    .

---
+++ sedmizadatak/PriceCalculatingVisitor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOONLV7
+{
+    class PriceCalculatingVisitor : IVisitor
+    {
+        public double Visit(DVD DVDItem)
+        {
+            if (DVDItem.Type == DVDType.SOFTWARE)
+            {
+                return DVDItem.Price;
+            }
+            else return DVDItem.Price + (DVDItem.Price * 0.1);
+        }
+        public double Visit(VHS VHSItem)
+        {
+            return VHSItem.Price + (VHSItem.Price * 0.1);
+        }
+        public double Visit(Book BookItem)
+        {
+            return BookItem.Price + (BookItem.Price * 0.1);
+        }
+
+    }
+}

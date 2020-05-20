43eaa96861cfc3c0e2e7ceeb2afd99a7f274c079
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 13:47:12 +0200

    .

---
+++ sestizadatak/IVisitor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPOONLV7
+{
+    interface IVisitor
+    {
+        double Visit(DVD DVDItem);
+        double Visit(VHS VHSItem);
+        double Visit(Book BookItem);
+    }
+}

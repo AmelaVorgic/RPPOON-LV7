dd1b9348c67a4c5849940386b79636194a0fbb84
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-05-20 12:29:28 +0200

    .

--- trecizadatak/SystemDataProvider.cs
+++ trecizadatak/SystemDataProvider.cs
@@ -18,11 +18,21 @@ namespace RPPOONLV7
         public float GetCPULoad()
         {
             float currentLoad = this.CPULoad;
+            if (currentLoad != this.previousCPULoad)
+            {
+                this.Notify();
+            }
+            this.previousCPULoad = currentLoad;
             return currentLoad;
         }
         public float GetAvailableRAM()
         {
             float currentLoad = this.AvailableRAM;
+            if (currentLoad != this.previousRAMAvailable)
+            {
+                this.Notify();
+            }
+            this.previousRAMAvailable = currentLoad;
             return currentLoad;
         }
     }
